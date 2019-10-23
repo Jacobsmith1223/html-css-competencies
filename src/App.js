f79ee36e8620c0a0ae388da3b67fed970a3e996e
Jake Smith <jacob_smith1223@me.com>
2019-10-23 14:19:33 -0600

    final css and html comps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='main-page'>
+        <header className='header'>
+          <h1>Jakes Competency Page</h1>
+          <p>Lorem Ipsum </p>
+        </header>
+        <section className='big-section'>
+          <article id='article'>
+            This is an article proving that I can section my HTML in the correct way. please pass this off 
+          </article>
+        </section>
+        <footer className='feet'>
+          <h1>LOOK! my webpage has feet</h1>
+        </footer>
+      </div>
+    )
+  }
 }
-
-export default App;
